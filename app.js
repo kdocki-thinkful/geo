363b32636ed516f9b5741382b50762cb6d7ab899
Paul Kavanagh <paulkav1@gmail.com>
2015-03-29 12:36:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-angular.module('myApp', ['ngRoute', 'ngAnimate', 'ngTable', 'angularSpinner'])
-.config(function($routeProvider){
+var app = angular.module('myApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap']);
+
+app.config(function($routeProvider){
 	$routeProvider
 	.when('/', {
 	    templateUrl : './home.html',
@@ -11,20 +12,25 @@ angular.module('myApp', ['ngRoute', 'ngAnimate', 'ngTable', 'angularSpinner'])
 	})
 	.when('/country/:countryCode', {
 	    templateUrl : './country.html',
-	    controller : 'ItemCtrl',
+	    controller : 'ItemCtrl', 
+	    resolve: {
+	    	countries: function(dataStore){
+		    	return dataStore.fetchCountries();
+		    }
+	    }
 	})
-})
-.run(function($rootScope, $location, $timeout) {
+});
+
+app.run(function($rootScope, $location) {
     $rootScope.$on('$routeChangeStart', function() {
         $rootScope.isLoading = true;
     });
     $rootScope.$on('$routeChangeSuccess', function() {
-      $timeout(function() {
         $rootScope.isLoading = false;
-      }, 1000);
     });
-})
-.factory("dataStore", function Data($rootScope, $http){
+});
+
+app.factory("dataStore", function Data($rootScope, $http){
 	var country = {};
 	var countries = [];
 	setCountry = function(countryName, countryCode, capital, area, pop, cont){
@@ -37,57 +43,68 @@ angular.module('myApp', ['ngRoute', 'ngAnimate', 'ngTable', 'angularSpinner'])
 		country.cont = cont;
 		countries.push(country);
 	};
-	fetchCountries = function(){
-		$rootScope.ajaxLoading = true;
-	    $http({method: 'GET', url: "http://api.geonames.org/countryInfoJSON?username=paulkav1"})
-	    .success(function(data, status, headers, config) {
-			for (var i = 0; i < data.geonames.length; i++){
-				setCountry(data.geonames[i].countryName, data.geonames[i].countryCode, data.geonames[i].capital, 
-					data.geonames[i].areaInSqKm, data.geonames[i].population, data.geonames[i].continent);
-				$rootScope.ajaxLoading = false;		
-			}			
-	    })
-	    .error(function(data, status, headers, config) {
-			alert('fail');
-			$rootScope.ajaxLoading = false;
-	    });
-	};
+
 	return {
+		fetchCountries: function(){
+			if ($rootScope.ajaxLoaded === true)
+				return;
+		    return $http({method: 'GET', url: "http://api.geonames.org/countryInfoJSON?username=paulkav1"})
+		    .success(function(data, status, headers, config) {
+				for (var i = 0; i < data.geonames.length; i++){
+					setCountry(data.geonames[i].countryName, data.geonames[i].countryCode, data.geonames[i].capital, 
+						data.geonames[i].areaInSqKm, data.geonames[i].population, data.geonames[i].continent);
+					$rootScope.ajaxLoaded = true;
+				}			
+		    })
+		    .error(function(data, status, headers, config) {
+				alert('fail');
+				$rootScope.ajaxLoaded = false;			
+		    });
+		},		
 		getCountry: function(cc){
-			if (countries.length === 0) fetchCountries();
 			for (var i = 0; i < countries.length; i++){
 				if (countries[i].countryCode === cc)		
 					break;
 			}
 			return countries[i];
 		},
-		getCountries: function(){
-			if (countries.length === 0) fetchCountries();			
-			return countries;
+		getCountries: function(start, chunk){
+			var first = (start - 1) * chunk;			
+			return countries.slice(first, first + chunk); //
 		},
-		getLength: function(){
-			if (countries.length === 0) fetchCountries();			
+		getLength: function(){		
 			return countries.length;
 		},
 	};
-})
-.controller('ListCtrl', function($scope, dataStore, ngTableParams) {
-	$scope.countries = dataStore.getCountries(); 
-	var len = dataStore.getLength();
-    $scope.tableParams = new ngTableParams({
-        page: 1,            // show first page
-        count: 25           // count per page
-    }, {
-        total: len,
-        getData: function($defer, params) {
-            $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-        }
-    });	   
-})		
-.controller('HomeCtrl', function($scope, usSpinnerService) {
-    usSpinnerService.spin('sp1');
-})
-.controller('ItemCtrl', function($scope, $http, $routeParams, dataStore) {
+});
+
+app.controller('ListCtrl', function($scope, dataStore) {
+ 	 $scope.currentPage = 1;
+ 	 $scope.chunk = 20;
+ 	 dataStore.fetchCountries();
+	 $scope.ajaxLoading = true; 	
+     setTimeout(function() {
+     	$scope.countries = dataStore.getCountries($scope.currentPage, $scope.chunk);
+		$scope.totalItems = dataStore.getLength(); 	 
+	    $scope.numPages = $scope.totalItems / $scope.chunk; 
+		$scope.ajaxLoading = false;	        	
+     	$scope.$apply();
+     }, 2000); 
+
+  	 $scope.setPage = function (pageNo) {
+     	$scope.currentPage = pageNo;
+ 	 	$rootScope.currentPage = $scope.currentPage;     	
+  	 };
+
+     $scope.pageChanged = function() {
+	 	$scope.countries = dataStore.getCountries($scope.currentPage, $scope.chunk);
+     }; 	    
+});
+
+app.controller('HomeCtrl', function($scope) {
+});
+
+app.controller('ItemCtrl', function($scope, $http, $routeParams, dataStore) {
 	$scope.cc = $routeParams.countryCode;
 	$scope.country = dataStore.getCountry($scope.cc);
 	$scope.cclc = $scope.cc.toLowerCase();
